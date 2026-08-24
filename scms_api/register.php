220f00cbef22ed7c9bfc9f08dfd27190258f1612
Jaimul Haque <jaimulhaque869@gmail.com>
2026-08-24 19:23:34 +0600

    Update register.php

--- scms_api/register.php
+++ scms_api/register.php
@@ -1,9 +1,4 @@
-<?php
-ob_start();
-header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, ngrok-skip-browser-warning");
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') { ob_end_clean(); http_response_code(200); exit; }
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') { ob_end_clean(); http_response_code(405); echo json_encode(["success"=>false,"message"=>"Method not allowed"]); exit; }
