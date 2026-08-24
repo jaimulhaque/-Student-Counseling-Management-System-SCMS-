7fc1522498d37b84271ed1f99680d327038309c6
Jaimul Haque <jaimulhaque869@gmail.com>
2026-08-24 19:22:20 +0600

    Update update_profile.php

--- scms_api/update_profile.php
+++ scms_api/update_profile.php
@@ -1,4 +1,9 @@
-
+<?php
+ob_start();
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, ngrok-skip-browser-warning");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') { ob_end_clean(); http_response_code(200); exit; }
 
